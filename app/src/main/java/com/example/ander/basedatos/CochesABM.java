a7f8e6c90442656faaf7347c964ac96608d4c1c7
ander69 <ander69>
2016-11-15 19:52:03 +0100

    casi terminado

--- app/src/main/java/com/example/ander/basedatos/CochesABM.java
+++ app/src/main/java/com/example/ander/basedatos/CochesABM.java
@@ -6,11 +6,15 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 //prueba 213487
 
 public class CochesABM extends AppCompatActivity {
@@ -31,7 +35,7 @@ public class CochesABM extends AppCompatActivity {
         spDni=(Spinner)findViewById(R.id.spDni);
         tvTitulo=(TextView)findViewById(R.id.tvTitulo);
         Bundle bundle= getIntent().getExtras();
-        String opcion= bundle.getString("opcion");
+        opcion= bundle.getString("opcion");
         Toast notificacion = Toast.makeText(this, opcion, Toast.LENGTH_SHORT);
         tvTitulo.setText(opcion.toUpperCase());
 
@@ -50,6 +54,39 @@ public class CochesABM extends AppCompatActivity {
             etPotencia.setEnabled(false);
             spDni.setEnabled(false);
         }
+        cargarSpinner();
+    }
+
+
+    public void cargarSpinner(){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "seguros", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+        List<String> listaDni = new ArrayList<>();
+        Cursor fila = db.rawQuery("SELECT dni FROM propietarios", null);
+
+        if(fila.getCount() <= 0) {
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item, new String[]{""});
+            spDni.setAdapter(adapter);
+            return;
+        }
+        fila.moveToFirst();
+        boolean primer = true;
+        do {
+            if(primer) {
+                primer = false;
+            } else {
+                fila.moveToNext();
+            }
+
+            String dni = fila.getString(0);
+            listaDni.add(dni);
+        } while(!fila.isLast());
+        fila.close();
+        db.close();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item, listaDni);
+        spDni.setAdapter(adapter);
+
+
     }
     public void guardar(View v){
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "seguros", null, 1);
@@ -59,7 +96,7 @@ public class CochesABM extends AppCompatActivity {
             String marca = etMarca.getText().toString();
             String potencia = etPotencia.getText().toString();
             String dni = spDni.getSelectedItem().toString();
-            Cursor fila = db.rawQuery("select marca,potencia,dni from coches where matricula=" + matricula, null);
+            Cursor fila = db.rawQuery("select marca,potencia,dni from coches where matricula='" + matricula + "'", null);
             if (fila.moveToFirst()) {
                 Toast.makeText(this, "Ya esta en la tabla", Toast.LENGTH_LONG).show();
             } else {
@@ -96,7 +133,7 @@ public class CochesABM extends AppCompatActivity {
             registro.put("marca", marca);
             registro.put("potencia", potencia);
             registro.put("dni", dni);
-            int seModifico = db.update("coches", registro, "matricula="+matricula,null);
+            int seModifico = db.update("coches", registro, "matricula='" + matricula + "'",null);
             if (seModifico == 1){
                 Toast.makeText(this,"se modifico",Toast.LENGTH_LONG).show();
 
@@ -110,11 +147,20 @@ public class CochesABM extends AppCompatActivity {
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"seguros", null, 1);
         SQLiteDatabase db = admin.getWritableDatabase();
         String matricula = etMatricula.getText().toString();
-        Cursor fila = db.rawQuery("select marca,potencia,dni from coches where matricula="+matricula,null);
+        Cursor fila = db.rawQuery("select marca,potencia,dni from coches where matricula='" + matricula + "'",null);
         if (fila.moveToFirst()){
-            etMarca.setText(fila.getString(0));
-            etPotencia.setText(fila.getString(1));
-            //spDni.setSelected(fila.get(2));
+            String marca = fila.getString(0);
+            String potencia = fila.getString(1);
+            String dni = fila.getString(2);
+            etMarca.setText(marca);
+            etPotencia.setText(potencia);
+            for(int i=0; i<spDni.getCount(); i++) {
+                if(spDni.getItemAtPosition(i).toString().equals(dni)) {
+                    spDni.setSelection(i, true);
+                    break;
+                }
+            }
+
             if (opcion.equals("modificar")){
                 etPotencia.setEnabled(true);
                 etMarca.setEnabled(true);
@@ -124,6 +170,7 @@ public class CochesABM extends AppCompatActivity {
             Toast.makeText(this,"no existe",Toast.LENGTH_SHORT).show();
 
         }
+        db.close();
     }
     public void volver (View v){
 
--- app/src/main/java/com/example/ander/basedatos/Consulta.java
+++ app/src/main/java/com/example/ander/basedatos/Consulta.java
@@ -1,14 +1,24 @@
 package com.example.ander.basedatos;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Consulta extends AppCompatActivity {
     private TextView tvTitulo,tvOpcion;
     private EditText etMatricula;
+    private ListView lvLista;
+    private  String opcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +27,80 @@ public class Consulta extends AppCompatActivity {
         tvTitulo=(TextView)findViewById(R.id.tvTitulo);
         tvOpcion=(TextView)findViewById(R.id.tvOpcion);
         etMatricula=(EditText)findViewById(R.id.etMatricula);
+        lvLista = (ListView) findViewById(R.id.lvLista);
         Bundle bundle = getIntent().getExtras();
-        String opcion=bundle.getString("opcion");
+        opcion=bundle.getString("opcion");
         tvTitulo.setText("CONSULTA "+opcion.toUpperCase());
         if (opcion.equals("propietarios")){
             tvOpcion.setText("DNI:");
+            AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "seguros", null, 1);
+            SQLiteDatabase db = admin.getWritableDatabase();
+            List<String> arrayPropietarios = new ArrayList<>();
+            Cursor fila = db.rawQuery("select nombre, edad, telefono, dni from propietarios", null);
+            if(fila.moveToFirst()) {
+                boolean primera = true;
+                do {
+                    if(!primera) {
+                        fila.moveToNext();
+                    }
+                    String texto = "Nombre: "+fila.getString(0)+", edad: "+fila.getString(1)+", telefono: "+fila.getString(2)+", DNI: "+fila.getString(3);
+                    arrayPropietarios.add(texto);
+
+                    if(primera) {
+                        primera = false;
+                    }
+                } while(!fila.isLast());
+            }
+            db.close();
+
+            ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayPropietarios);
+            lvLista.setAdapter(adaptador);
         }
         if (opcion.equals("coches")){
             tvOpcion.setText("Matricula:");
+            AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "seguros", null, 1);
+            SQLiteDatabase db = admin.getWritableDatabase();
+            List<String> arrayCoches = new ArrayList<>();
+            Cursor fila = db.rawQuery("select matricula, marca, potencia, dni from coches", null);
+            if(fila.moveToFirst()) {
+                boolean primera = true;
+                do {
+                    if(!primera) {
+                        fila.moveToNext();
+                    }
+
+                    String texto = "Matricula: "+fila.getString(0)+", Marca: "+fila.getString(1)+", Potencia: "+fila.getString(2)+", DNI: "+fila.getString(3);
+                    arrayCoches.add(texto);
+
+                    if(primera) {
+                        primera = false;
+                    }
+                } while(!fila.isLast());
+            }
+            db.close();
+
+            ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayCoches);
+            lvLista.setAdapter(adaptador);
+        }
+
+    }
+
+    public void buscar(View v){
+
+        if (opcion.equals("coches")){
+            Toast.makeText(this,"aqui",Toast.LENGTH_SHORT).show();
+            AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"seguros", null, 1);
+            SQLiteDatabase db = admin.getWritableDatabase();
+            String matricula = etMatricula.getText().toString();
+            Cursor fila = db.rawQuery("select marca,potencia,dni from coches where matricula='" + matricula + "'",null);
+            if (fila.moveToFirst()) {
+                for (int i = 0; i < lvLista.getCount(); i++) {
+                    if (lvLista.getItemAtPosition(i).toString().equals(matricula)) {
+                        lvLista.setSelection(i);
+                        break;
+                    }
+                }
+            }
         }
 
     }
